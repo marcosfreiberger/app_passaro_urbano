c68d3341d71a5f273a6b9b36fab8bbac2caa6aa9
Marcosnfrei <marcosnfrei@gmail.com>
2020-09-01 23:26:04 -0300

    primeiro commit

---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
+import { CarrinhoService } from '../carrinho.service';
+import { ItemCarrinho } from '../shared/item-carrinho.model';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
+})
+
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  public oferta: Oferta
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService,
+    private carrinhoService: CarrinhoService
+    ) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe((parametros: Params) => {
+      this.ofertasService.getOfertaPorId(parametros.id)
+      .then(( oferta: Oferta ) => {
+        this.oferta = oferta
+      })
+    })
+ 
+  }
+
+  ngOnDestroy() {
+  }
+
+  public adicionarItemCarrinho(oferta: Oferta): void {
+    this.carrinhoService.incluirItem(this.oferta)
+    console.log(this.carrinhoService.exibirItens())
+  }
+
+}
---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Observable } from 'rxjs/Observable';
+import { Oferta } from '../shared/oferta.model';
+import { Subject } from 'rxjs/Subject';
+
+import '../util/rxjs-extensions'
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
+})
+export class TopoComponent implements OnInit {
+
+  public ofertas: Observable<Oferta[]>
+
+  private subjectPesquisa: Subject<string> = new Subject<string>()
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+      .debounceTime(1000) //executa a acão do switchMap após 1 segundo
+      .distinctUntilChanged() //para fazer pesquisas distintas
+      .switchMap((termo: string) => {
+        if(termo.trim() === '') {
+          //retornar um Observable de array de ofertas vazio
+          return Observable.of<Oferta[]>([])
+        }
+        return this.ofertasService.pesquisaOfertas(termo)
+      })
+      .catch((err: any) => {
+        return Observable.of<Oferta[]>([])
+      })
+  }
+
+  public pesquisa(termoDaBusca: string): void {
+    this.subjectPesquisa.next(termoDaBusca)
+  }
+
+  public limpaPesquisa(): void {
+    this.subjectPesquisa.next('')
+  }
+
+}
